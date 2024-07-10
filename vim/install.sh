e9a362410a58e373d0aead1fdf77c90ff8e71204
Msfv3n0m <jlbieker32@gmail.com>
2024-07-10 13:51:14 -0400

    add install.sh

---
+++ vim/install.sh
@@ -0,0 +1,8 @@
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
+sudo -E bash nodesource_setup.sh
+sudo apt-get install -y nodejs
+node -v
+cp ./.vimrc ~/.vimrc
+vim -c "source ~/.vimrc" -c "PlugInstall"
