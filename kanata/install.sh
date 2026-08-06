1306c06d7b222fc62f9f816ee9b5c358b3e76b50
Jared Bieker <71530214+Msfv3n0m@users.noreply.github.com>
2026-08-05 22:33:18 -0500

    update install

--- kanata/install.sh
+++ kanata/install.sh
@@ -1,7 +1,7 @@
-sudo su 
 mkdir -p /usr/bin/kanata
 curl https://github.com/jtroo/kanata/releases/download/v1.12.0/linux-binaries-x64.zip -O kanata.zip
-unzip kanata.zip /usr/bin/kanata/
+unzip kanata.zip 
+mv $(pwd)/kanata_linux_x64 /usr/bin/kanata/
 mv $(pwd)/kanata.kbd /usr/bin/kanata/
 cp $(pwd)/kanata.service /etc/systemd/system/
 systemctl daemon-reload
